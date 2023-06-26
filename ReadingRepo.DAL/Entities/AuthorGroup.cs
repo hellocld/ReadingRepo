26e06a6fbbc967e617cdd5df88bd85b31d659ab9
Chris Langford <chris@hellocld.com>
2023-06-26 14:47:08 -0400

    Create dummy startup project for EF migration. Define primary key in authorgroup model

--- ReadingRepo.DAL/Entities/AuthorGroup.cs
+++ ReadingRepo.DAL/Entities/AuthorGroup.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +7,10 @@ using System.Threading.Tasks;
 
 namespace ReadingRepo.DAL.Entities
 {
+    [PrimaryKey(nameof(GroupId), nameof(AuthorId))]
     public class AuthorGroup
     {
+
         public Guid GroupId { get; set; }
         public Guid AuthorId { get; set; }
     }
---
+++ ReadingRepo.DummyStartup/ReadMe.md
@@ -0,0 +1,5 @@
+# DummyStartup
+
+This project is required in order to run the EF Core CLI tools, which in turn are used to create DB migrations. 
+
+Android doesn't have the necessary .NET class library the tools require. For more info, see: https://learn.microsoft.com/en-us/ef/core/cli/powershell#other-target-frameworks
